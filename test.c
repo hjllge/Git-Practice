c97f34095a6a36cbcf3bf12cf6222f76b6718136
HJ <eguswhd@naver.com>
2020-03-10 15:10:17 +0900

    Branch test 2

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	char *str = "Hello world!, I'm test branch!";
+	char *str = "Hello world!, I'm test branch! I will push without merging!";
 	printf("%s\n", str);
 	return 0;
 }
